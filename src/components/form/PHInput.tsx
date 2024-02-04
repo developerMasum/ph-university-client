4be82a2bae248c98ca9b9fd1a37b6da0225f4528
developerMasum <developer.masum2001@gmail.com>
2024-02-04 22:25:19 +0600

    added

--- src/components/form/PHInput.tsx
+++ src/components/form/PHInput.tsx
@@ -1,5 +1,5 @@
-import { Input } from 'antd';
-import { Controller } from 'react-hook-form';
+import { Form, Input } from "antd";
+import { Controller } from "react-hook-form";
 
 type TInputProps = {
   type: string;
@@ -9,11 +9,14 @@ type TInputProps = {
 
 const PHInput = ({ type, name, label }: TInputProps) => {
   return (
-    <div style={{ marginBottom: '20px' }}>
-      {label ? label : null}
+    <div style={{ marginBottom: "20px" }}>
       <Controller
         name={name}
-        render={({ field }) => <Input {...field} type={type} id={name} />}
+        render={({ field }) => (
+          <Form.Item label={label}>
+            <Input {...field} type={type} id={name} />
+          </Form.Item>
+        )}
       />
     </div>
   );
---
+++ src/constants/global.ts
@@ -0,0 +1,18 @@
+export const monthsName = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+export const monthOptions = monthsName.map((item) => ({
+  value: item,
+  label: item,
+}));
