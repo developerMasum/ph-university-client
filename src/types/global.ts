64eb0cfca79c3a2ebb0001d2fd3ec12b4a186ec2
developerMasum <developer.masum2001@gmail.com>
2024-02-06 00:24:02 +0600

    added till academic management

--- src/types/global.ts
+++ src/types/global.ts
@@ -1,12 +1,30 @@
+import { BaseQueryApi } from "@reduxjs/toolkit/query";
+
 export type TError = {
   data: {
     message: string;
     stack: string;
     success: boolean;
   };
-  status: number
+  status: number;
 };
-export type TResponse  = {
-    data?: any;
-    error?: TError;
-}
+export type TResponse<T> = {
+  data?: T;
+  error?: TError;
+  meta?: TMeta;
+  success?: boolean;
+};
+
+export type TMeta = {
+  limit: number;
+  page: number;
+  total: number;
+  totalPage: number;
+};
+
+export type TQueryParam = {
+  name: string;
+  value: boolean | React.Key;
+};
+
+export type TResponseRedux<T> = TResponse<T> & BaseQueryApi;
